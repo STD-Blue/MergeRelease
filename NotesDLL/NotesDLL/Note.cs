41a68553e148fa8fd24e55820ac20de413ea7fd0
margo <student@25-03>
2021-07-02 11:07:43 +0300

    Add class Note

---
+++ NotesDLL/NotesDLL/Note.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesDLL
+{
+    public class Note
+    {
+        public string NoteName;
+        public string Text;
+        public DateTime CreationData;
+        public Note()
+        {
+            NoteName = string.Empty;
+            Text = string.Empty;
+            CreationData = new DateTime();
+        }
+        public Note(string noteName, string text, DateTime creationData)
+        {
+            Text = text;
+            NoteName = noteName;
+            CreationData = creationData;
+        }
+    }
+}
