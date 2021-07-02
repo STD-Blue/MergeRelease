afd4dc0e23afdac78fe1e3f88146ee87fc8dbb5a
Max <student@25-06>
2021-07-02 11:23:53 +0300

    Added Note File Operations class and realized body of methods

--- NotesDLL/NotesDLL/Class1.cs
+++ NotesDLL/NotesDLL/Class1.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace NotesDLL
 {
-    public class Class1
+    public class Note { }
+    public static class NoteFileOperations
     {
+        public static bool SaveNote(Note note)
+        {
+            bool res = false;
+            if (note != null)
+            {
+
+            }
+            return res;
+        }
+        public static bool SaveNotes(params Note[] notes)
+        {
+            bool res = false;
+            if (notes.Where(x => x != null).Count() == notes.Length)
+            {
+
+            }
+            return res;
+        }
     }
 }
