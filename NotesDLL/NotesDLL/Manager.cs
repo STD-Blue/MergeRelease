7158835efa0b9fe7d097c59ec33d90843784d1f9
margo <student@25-03>
2021-07-02 11:43:57 +0300

    Add class Manager

---
+++ NotesDLL/NotesDLL/Manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesDLL
+{
+    public class Manager
+    {
+        public Note note { get; private set; }
+        public Manager()
+        {
+            note = new Note();
+        }
+        public Manager(string noteName, string text, DateTime creationData)
+        {
+            this.note = new Note(noteName, text, creationData);
+        }
+    }
+}
